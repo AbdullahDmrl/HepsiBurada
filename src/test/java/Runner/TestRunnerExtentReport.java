a7672c891a2342992bc2d482e54271c435365c6d
ademi <ademirel1971@yahoo.com>
2021-10-11 18:17:26 +0200

    basit report

--- src/test/java/Runner/TestRunnerExtentReport.java
+++ src/test/java/Runner/TestRunnerExtentReport.java
@@ -9,23 +9,12 @@ import org.testng.annotations.AfterClass;
         features = {"src/test/java/FeatureFiles"},
         glue = {"StepDefinations"},
 
-        plugin = {
-                "com.cucumber.listener.ExtentCucumberFormatter:target/ExtentReport/ExtentReportRegression.html"
-                // oluşturalacak raporun yeri ve adı veriliyor.
+        plugin = { //basit rapor oluşturan plugin
+                "html:target/cucumber-report",
+                "json:target/cucumber.json",
         }
 )
 public class TestRunnerExtentReport extends AbstractTestNGCucumberTests {
 
-       @AfterClass
-       public static void afterClass() {
-           Reporter.loadXMLConfig("src/test/java/XML_Files/ExtentReportSet.xml");
 
-           Reporter.setSystemInfo("User Name", "A Demirel");
-           Reporter.setSystemInfo("Application Name", "Basqar");
-           Reporter.setSystemInfo("Operating System Info", System.getProperty("os.name").toString());
-           Reporter.setSystemInfo("Department", "QAr");
-           Reporter.setSystemInfo("Ek Satır", "Acıklamasi");
-
-           Reporter.setTestRunnerOutput("Test Execution Cucumber Report");
-       }
 }
